ea2a9a9d27e00274d64576aaf7a66e99e2ba3339
MDzahin <afsarzahin@gmail.com>
2019-09-15 19:16:11 +0600

    updated

---
+++ game.js
@@ -0,0 +1,128 @@
+
+
+var stbtn = document.getElementById("stopbtn");
+var size = (outerWidth/2)-70;
+var rock =document.getElementById('rocket');
+var fr =document.getElementById('fire');
+var enemi =document.getElementById('enemi');
+var movenemi = 0;
+var trax = 0;
+
+function enemi1(){
+var id = setInterval(framenemi, 2000);
+      function framenemi() {
+      var width = ((outerWidth-45)/10)*((Math.random())*10);
+      enemi.style.transform = "translateX(" + (width) + "px)";
+      var scl = scl+0.05;
+      enemi.style.transform = "scale(" + (scl) + ")";
+    }
+}
+
+// function enemi2(){
+// var width = outerWidth-40;
+// var id = setInterval(framenemi, 20);
+//   function framenemi() {
+//     if (movenemi <= 0) {
+//       enemi1();
+//     } else {
+//       movenemi=movenemi-5;
+//       enemi.style.transform = "translateX(" + (movenemi) + "px)";
+//     }
+// }
+// }
+
+function enemidown(){
+  var top = 20;
+  var id = setInterval(framenemi, 2000);
+  function framenemi() {
+    if (top >= outerHeight-300) {
+      clearInterval(id);
+      alert('Game Over,refrash the window to start again');
+    } else {
+      top=top+20;
+      enemi.style.top = "" + (top) + "px";
+    }
+}
+}
+  
+function close(){
+  if (confirm("Close Window?")) {
+    close();
+  }
+}
+
+function go(){
+  var game = document.getElementById("option");
+  var main = document.getElementById("gamebody");
+  game.style.display="none";
+  main.style.display="block";
+  enemi1();
+  enemidown();
+};
+
+function stop(){
+  fr.style.transform=rock.style.transform;
+  fr.style.display="block";
+  fire();
+
+}
+
+
+
+ function fire(){
+  var tray = 120;
+  var height=(outerHeight+120);
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (tray >= height) {
+      clearInterval(id);
+      fr.style.bottom = "120px";
+      fr.style.display="none";
+      tray = 120;
+    } else {
+      stbtn.addEventListener("click", function(){
+    clearInterval(id);
+    fr.style.bottom= "120px";
+    });
+      tray=tray+7;
+      fr.style.bottom = "" + (tray) + "px";
+    }
+}
+}
+
+
+  
+  function left() {
+  var id = setInterval(framef, 15);
+  function framef() {
+    if (trax >= size) {
+      clearInterval(id);
+    } else {
+    stbtn.addEventListener("click", function(){
+    clearInterval(id);
+    });
+      trax=trax+5;
+      rock.style.transform = "translateX(" + (trax) + "px)";
+    }
+}
+}
+
+
+
+
+
+
+function right() {
+  var id = setInterval(framel, 15);
+  function framel() {
+    if (trax <= -size-70) {
+      clearInterval(id);
+    } else {
+    stbtn.addEventListener("click", function(){
+    clearInterval(id);
+    });
+      trax=trax-5;
+      rock.style.transform = "translateX(" + (trax) + "px)";
+    }
+}
+}
