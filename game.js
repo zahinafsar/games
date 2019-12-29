9eaaa72717969b06518bfbfff76d4bc8662dddb7
Zahin Afsar <43959210+Mdzahin@users.noreply.github.com>
2019-12-29 06:56:52 -0800

    Update game.js

--- game.js
+++ game.js
@@ -33,11 +33,11 @@ var win = document.getElementById("win");
 
 // score
 var s = document.querySelector('#sr');
-var stop = document.querySelector('#srtop');
+var stp = document.querySelector('#srtop');
 
 //high score
 var sh = document.querySelector('#srh');
-var shtop = document.querySelector('#srhtop');
+var shtp = document.querySelector('#srhtop');
 var movenemi = 0;
 var trax = 0;
 var z = 0;
@@ -92,7 +92,7 @@ localStorage.setItem("best",100)
 // start new game
 function go(){
   if (localStorage.length!=0) {
-  shtop.innerText = parseInt(localStorage.getItem("best"));
+  shtp.innerText = parseInt(localStorage.getItem("best"));
   }
   if (game.style.display=="block") {
   enemi.style.display = "block";
@@ -207,7 +207,7 @@ var enmtp = enemi.getBoundingClientRect().top;
 var frlf = fr.getBoundingClientRect().left;
 var frtp = fr.getBoundingClientRect().top;
 if(Math.abs(Math.abs(enmlf)-Math.abs(frlf))<50 && Math.abs(Math.abs(enmtp)-Math.abs(frtp))<5){
-  stop.innerText = z;
+  stp.innerText = z;
   enemi.style.width="0px";
   killed = true;
   fr.style.bottom = "120px";
